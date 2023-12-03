330520a8b795d31482df0a428420186fa6367285
Sammy Joseph <sam.tab.paz@gmail.com>
2023-12-03 18:53:26 -0500

    clase #6 - 20:45

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Route::prefix('v1')->resource('clase-6', NoteApiController::class);
+// Route::resource('clase-6', NoteApiController::class);
+/* Route::group(['prefix' => 'v1'], function () {
+    Route::resource('clase-6', NoteApiController::class);
+}); */
+
+Route::prefix('v1')->group(function () {
+    Route::resource('clase-6', NoteApiController::class);
 });
