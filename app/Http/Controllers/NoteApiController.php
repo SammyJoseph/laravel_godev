e049fed76041e6141e56f3c6845f4433afe71d7a
Sammy Joseph <sam.tab.paz@gmail.com>
2023-12-04 14:23:32 -0500

    clase #6 - 35:07

--- app/Http/Controllers/NoteApiController.php
+++ app/Http/Controllers/NoteApiController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\NoteApiRequest;
+use App\Models\Note;
+use App\Models\NoteApi;
 use Illuminate\Http\Request;
 
 class NoteApiController extends Controller
@@ -11,7 +14,9 @@ class NoteApiController extends Controller
      */
     public function index()
     {
-        //
+        $notes = Note::all();
+
+        return response()->json($notes, 200);
     }
 
     /**
@@ -19,23 +24,27 @@ class NoteApiController extends Controller
      */
     public function create()
     {
-        //
+        // no se utiliza en API
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(NoteApiRequest $request)
     {
-        //
+        Note::create($request->all());
+
+        return response()->json([
+            'success' => true
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(NoteApi $note)
     {
-        //
+        return response()->json($note, 200);
     }
 
     /**
@@ -43,22 +52,30 @@ class NoteApiController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        // no se utiliza en API
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(NoteApiRequest $request, NoteApi $note)
     {
-        //
+        $note->update($request->all());
+
+        return response()->json([
+            'success' => true,
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(NoteApi $note)
     {
-        //
+        $note->delete();
+
+        return response()->json([
+            'success' => true,
+        ], 200);
     }
 }
