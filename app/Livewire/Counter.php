afdf8e5b6132f639c22fd97df067f29838d94e66
SammyJoseph <sam.tab.paz@gmail.com>
2024-01-19 17:52:15 -0500

    clase #11 - 1:07:27

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -7,6 +7,12 @@ use Livewire\Component;
 class Counter extends Component
 {
     public $count = 0;
+    public $username = '';
+
+    public function mount() // mount es un hook que se ejecuta en cierta etapa (después del renderizado)
+    {
+        $this->fill(['count' => 20]);
+    }
 
     public function render()
     {
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,8 +1,15 @@
 <div class="p-6">
-    <p class="text-3xl">{{ $count }}</p>
-    <button
-        wire:click="increment"
-        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-2">
-        Button
-    </button>
+    <div>
+        <p class="text-3xl">{{ $count }}</p>
+        <button
+            wire:click="increment"
+            class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-2">
+            Button
+        </button>
+    </div>
+    <hr class="my-8">
+    <div>    
+        <input type="text" wire:model.live="username">
+        <p class="text-3xl">{{ $username }}</p>
+    </div>
 </div>
